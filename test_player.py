df04569902ad6e883df045e705c66c2e325253ef
hmerl <65544389+hmerl@users.noreply.github.com>
2022-05-01 09:13:28 +0200

    added testcase for equality

--- test_player.py
+++ test_player.py
@@ -7,3 +7,11 @@ def test_playercreation():
     assert oneplayer.category==1
     assert oneplayer.currentid>oneplayer.id
 
+def test_playercomparison():
+    firstplayer=player.Player(1)
+    secondplayer=player.Player(1)
+
+    assert firstplayer==firstplayer
+    assert secondplayer==secondplayer
+    assert firstplayer!=secondplayer
+    assert secondplayer!=firstplayer
